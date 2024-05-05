6b7ed6a84219008de2cb81884f3d0b093fe1fbdd
Patrick Gehrsitz <mryel00.github@gmail.com>
2024-05-06 01:27:22 +0200

    Initial commit
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+time ./scripts/zmk_build.sh --host-config-dir /mnt/d/Programming_Projects/Cloned_Projects/zmk-config -o /mnt/d/Programming_Projects/Cloned_Projects/zmk-config/firmware_build
